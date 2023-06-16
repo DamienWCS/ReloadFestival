be555a2cf9e6226374e623e06d0192fc623bf8c8
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-06-16 10:05:20 +0200

    homepage creation

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import "./HomePage.scss";
 
 function HomePage() {
-  const mainArtists = [];
-
   return (
     <body>
       <h1>Reload Fest</h1>
-      <div class="info">
+      <div className="info">
         <p>19 & 20th August 2023</p>
         <p>Les Minimes</p>
         <p>Larochelle</p>
@@ -15,7 +13,7 @@ function HomePage() {
         src="https://images.unsplash.com/photo-1582711012124-a56cf82307a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1240&q=80"
         alt="Crowd in front of a stage"
       />
-      <div class="mainArtists">
+      <div className="mainArtists">
         *****A changer: têtes d'affiche du festival en animation défilante
       </div>
     </body>
