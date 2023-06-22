2106a44c06cc70d2ee60791a73ec1acab258696c
DamienWCS <108578437+DamienWCS@users.noreply.github.com>
2023-06-22 19:41:49 +0200

    Add filter system

--- frontend/src/pages/LineUp.jsx
+++ frontend/src/pages/LineUp.jsx
@@ -1,82 +1,174 @@
+import { useState } from "react";
 import "../styles/lineup.scss";
 
 function LineUp() {
+  const [filter, setFilter] = useState("");
+  const [dayFilter, setDayFilter] = useState("All");
+  const [stageFilter, setStageFilter] = useState("All");
+  const [showAllArtists, setShowAllArtists] = useState(false);
+
+  const data = [
+    {
+      day: "Saturday",
+      stage: "Stage 1",
+      artistes: [
+        "Armin van Buuren",
+        "Calvin Harris",
+        "Hardwell",
+        "Illenium",
+        "Kygo",
+        "Martin Garrix",
+        "Nicky Romero",
+        "Quintino",
+        "Skrillex",
+        "Tiesto",
+      ],
+    },
+    {
+      day: "Saturday",
+      stage: "Stage 2",
+      artistes: [
+        "Afrojack",
+        "Benni Benassi",
+        "Jax Jones",
+        "Paul Kalkbrenner",
+        "Disclosure",
+        "Excision",
+        "Deadmau5",
+        "Dr Peacock",
+        "Charlotte de White",
+        "DJ Snake",
+      ],
+    },
+    {
+      day: "Sunday",
+      stage: "Stage 3",
+      artistes: [
+        "Dash Berlin",
+        "Nora En Pure",
+        "Marshmello",
+        "Zedd",
+        "Alesso",
+        "Major Lazer",
+        "The Chainsmokers",
+        "Sickick",
+        "Deorro",
+        "Timmy Trumpet",
+      ],
+    },
+    {
+      day: "Sunday",
+      stage: "Stage 4",
+      artistes: [
+        "Sound of Legend",
+        "Da Tweekaz",
+        "Sub Zero Project",
+        "R3hab",
+        "Ummet Ozcan",
+        "Les Gordon",
+        "NTO",
+        "NHYX",
+        "Alle Farben",
+        "Marcapasos",
+      ],
+    },
+  ];
+
+  const handleDayFilter = (day) => {
+    if (dayFilter === day) {
+      setDayFilter("All");
+    } else {
+      setDayFilter(day);
+    }
+  };
+
+  const handleAllArtists = () => {
+    setShowAllArtists(!showAllArtists);
+  };
+
+  let artists;
+  if (showAllArtists) {
+    artists = [...new Set(data.flatMap(({ artistes }) => artistes))].sort();
+  }
+
   return (
     <div className="body">
       <div className="lineup-block">
         <h2>LINEUP</h2>
       </div>
       <div className="box-filter">
-        <input className="input" type="search" name="" id="" />
-        <button className="button-artistes" type="submit">
+        <input
+          className="input"
+          type="search"
+          placeholder="Nom de l' artiste"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+        <button
+          className={`button-artistes ${
+            showAllArtists ? "active-artistes" : ""
+          }`}
+          type="submit"
+          onClick={handleAllArtists}
+        >
           Artistes
         </button>
-        <button className="button-stage" type="submit">
-          Stage
-        </button>
-        <button className="button-day" type="submit">
+        <select
+          className="button-stage"
+          onChange={(e) => setStageFilter(e.target.value)}
+        >
+          <option value="All">All Stages</option>
+          <option value="Stage 1">Stage 1</option>
+          <option value="Stage 2">Stage 2</option>
+          <option value="Stage 3">Stage 3</option>
+          <option value="Stage 4">Stage 4</option>
+        </select>
+        <button
+          className={`button-day ${
+            dayFilter === "Saturday" ? "active-saturday" : ""
+          }`}
+          type="submit"
+          onClick={() => handleDayFilter("Saturday")}
+        >
           Saturday
         </button>
-        <button className="button-day" type="submit">
+        <button
+          className={`button-day ${
+            dayFilter === "Sunday" ? "active-sunday" : ""
+          }`}
+          type="submit"
+          onClick={() => handleDayFilter("Sunday")}
+        >
           Sunday
         </button>
       </div>
       <div className="stage_content">
-        <h4>Stage 1</h4>
-        <ul>
-          <li>Armin van Buuren</li>
-          <li>Calvin Harris</li>
-          <li>Hardwell</li>
-          <li>Illenium</li>
-          <li>Kygo</li>
-          <li>Martin Garrix</li>
-          <li>Nicky Romero</li>
-          <li>Quintino</li>
-          <li>Skrillex</li>
-          <li>Tiesto</li>
-        </ul>
-
-        <h4>Stage 2</h4>
-        <ul>
-          <li>Afrojack</li>
-          <li>Benni Benassi</li>
-          <li>Jax Jones</li>
-          <li>Paul Kalkbrenner</li>
-          <li>Disclosure</li>
-          <li>Excision</li>
-          <li>Deadmau5</li>
-          <li>Dr Peacock</li>
-          <li>Charlotte de White</li>
-          <li>DJ Snake</li>
-        </ul>
-
-        <h4>Stage 3</h4>
-        <ul>
-          <li>Dash Berlin</li>
-          <li>Nora En Pure</li>
-          <li>Marshmello</li>
-          <li>Zedd</li>
-          <li>Alesso</li>
-          <li>Major Lazer</li>
-          <li>The Chainsmokers</li>
-          <li>Sickick</li>
-          <li>Deorro</li>
-          <li>Timmy Trumpet</li>
-        </ul>
-
-        <h4>Stage 4</h4>
-        <ul>
-          <li>Sound of Legend</li>
-          <li>Da Tweekaz</li>
-          <li>Sub Zero Project</li>
-          <li>R3hab</li>
-          <li>Ummet Ozcan</li>
-          <li>Les Gordon</li>
-          <li>NTO</li>
-          <li>NHYX</li>
-          <li>Alle Farben</li>
-          <li>Marcapasos</li>
-        </ul>
+        {showAllArtists
+          ? artists
+              .filter((artist) =>
+                artist.toLowerCase().includes(filter.toLowerCase())
+              )
+              .map((artist) => <li key={artist}>{artist}</li>)
+          : data
+              .filter(
+                ({ day, stage }) =>
+                  (dayFilter === "All" || day === dayFilter) &&
+                  (stageFilter === "All" || stage === stageFilter)
+              )
+              .map(({ stage, artistes }) => (
+                <div key={stage}>
+                  <h4>{stage}</h4>
+                  <ul>
+                    {artistes
+                      .filter((artist) =>
+                        artist.toLowerCase().includes(filter.toLowerCase())
+                      )
+                      .map((artist) => (
+                        <li key={artist}>{artist}</li>
+                      ))}
+                  </ul>
+                </div>
+              ))}
       </div>
     </div>
   );
