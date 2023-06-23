0e0afd5b856d94f5ac2ea11ac9af04e72b878cc9
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-06-23 14:51:25 +0200

    festival schedule

---
+++ frontend/src/pages/Schedule.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+
+import "../styles/Schedule.scss";
+
+function Schedule() {
+  return (
+    <div className="schedule">
+      <h1>Schedule</h1>
+      <div className="day">
+        <h2>Saturday</h2>
+        <div className="scene">
+          <h3>Flying High</h3>
+          <div className="show">
+            <p className="artist">Illenium</p>
+            <p className="showTime">14:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Kygo</p>
+            <p className="showTime">15:30</p>
+          </div>
+          <div className="show">
+            <p className="artist">Skrillex</p>
+            <p className="showTime">20:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Armin Van Buuren</p>
+            <p className="showTime">21:30</p>
+          </div>
+          <div className="show">
+            <p className="artist">Hardwell</p>
+            <p className="showTime">23:00</p>
+          </div>
+        </div>
+        <div className="scene">
+          <h3>Sonic Sphere</h3>
+          <div className="show">
+            <p className="artist">Afrojack</p>
+            <p className="showTime">11:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Benny Benassi</p>
+            <p className="showTime">14:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Jax Jones</p>
+            <p className="showTime">16:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Paul Kalkbrenner</p>
+            <p className="showTime">18:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Disclosure</p>
+            <p className="showTime">21:00</p>
+          </div>
+        </div>
+        <div className="scene">
+          <h3>Electronic Dawn</h3>
+          <div className="show">
+            <p className="artist">Dash Berlin</p>
+            <p className="showTime">12:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Nora En Pure</p>
+            <p className="showTime">14:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Marshmello</p>
+            <p className="showTime">17:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Zedd</p>
+            <p className="showTime">20:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Alesso</p>
+            <p className="showTime">22:00</p>
+          </div>
+        </div>
+        <div className="scene">
+          <h3>Sunset Stage</h3>
+          <div className="show">
+            <p className="artist">Sound of Legend</p>
+            <p className="showTime">11:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Da Tweekaz</p>
+            <p className="showTime">15:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Sub Zero Project</p>
+            <p className="showTime">18:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">R3HAB</p>
+            <p className="showTime">21:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Ummet Ozcan</p>
+            <p className="showTime">23:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="day">
+        <h2>Sunday</h2>
+        <div className="scene">
+          <h3>Flying High</h3>
+          <div className="show">
+            <p className="artist">Nicky Romeo</p>
+            <p className="showTime">14:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Quintino</p>
+            <p className="showTime">15:30</p>
+          </div>
+          <div className="show">
+            <p className="artist">Tiesto</p>
+            <p className="showTime">20:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Martin Garrix</p>
+            <p className="showTime">21:30</p>
+          </div>
+          <div className="show">
+            <p className="artist">Calvin Harris</p>
+            <p className="showTime">23:00</p>
+          </div>
+        </div>
+        <div className="scene">
+          <h3>Sonic Sphere</h3>
+          <div className="show">
+            <p className="artist">DJ Snake</p>
+            <p className="showTime">11:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Charlotte de Witte</p>
+            <p className="showTime">14:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Dr Peacock</p>
+            <p className="showTime">16:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Excision</p>
+            <p className="showTime">18:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Deadmau5</p>
+            <p className="showTime">21:00</p>
+          </div>
+        </div>
+        <div className="scene">
+          <h3>Electronic Dawn</h3>
+          <div className="show">
+            <p className="artist">Major Lazer</p>
+            <p className="showTime">12:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">The Chainsmokers</p>
+            <p className="showTime">14:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Sickick</p>
+            <p className="showTime">17:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Deorro</p>
+            <p className="showTime">20:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Timmy Trumpet</p>
+            <p className="showTime">22:00</p>
+          </div>
+        </div>
+        <div className="scene">
+          <h3>Sunset Stage</h3>
+          <div className="show">
+            <p className="artist">Les gordon</p>
+            <p className="showTime">11:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">NTO</p>
+            <p className="showTime">15:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">NHYX</p>
+            <p className="showTime">18:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">AllE Farben</p>
+            <p className="showTime">21:00</p>
+          </div>
+          <div className="show">
+            <p className="artist">Marcapasos</p>
+            <p className="showTime">23:00</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Schedule;
